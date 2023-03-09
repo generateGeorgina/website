f016f0526aa5352b87ee889bf299443538f034bc
Georgina Appeagyei <appeagyeigeorgina@gmail.com>
2023-03-09 18:08:32 +0000

    add footer links

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
 let websiteData = {
     "hobby page": {
-        url: "PLACEHOLDER" 
+        url: "https://generategeorgina.github.io/hobby-page/" 
     },
     "project gallery": {
-        url: "PLACEHOLDER"
+        url: "https://generategeorgina.github.io/project-gallery/"
     },
     "movie data": {
-        url: "PLACEHOLDER"
+        url: "https://github.com/generateGeorgina.github.io/movie-data"
     },
     "website": {
-        url: "PLACEHOLDER"
+        url: "https://generategeorgina.github.io/website/"
     },
     "game": {
         url: "PLACEHOLDER"
@@ -23,10 +23,27 @@ console.log(Object.keys(websiteData));
 const ulFooterElement = document.getElementById('footer')
 
 for (let websiteName in websiteData) {
-    let liElement = document.createElement('a');
+    let liElement = document.createElement('li');
+    // console.log(liElement);
+    // console.log(ulFooterElement);
+    // nest li element in ul element
+    ulFooterElement.appendChild(liElement);
+    // create text
     let liWebsiteName = document.createTextNode(websiteName);
+    // let liWebsiteName = document.createTextNode("hello");
     console.log(websiteName);
     console.log(liWebsiteName);
-    liElement.appendChild(liWebsiteName);
-    liElement.setAttribute.href = websiteName['url']; //don't forget quote marks otherwise you get a reference error
-}
+    // create anchor element
+    const anchorElement = document.createElement('a')
+    // nest anchor element in li element
+    liElement.appendChild(anchorElement);
+    // add text to anchor element
+    anchorElement.appendChild(liWebsiteName);
+    // create an array for every website object
+    websiteArray = websiteData[websiteName];
+    for (let i in websiteArray) {
+        //add link
+        anchorElement.href = websiteArray[i];
+    }
+    
+};
